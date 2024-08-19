0446f16bcc4f607a90249ac7df87dc2a514188d8
hound672 <hound672@gmail.com>
2024-08-19 11:20:40 +0300

    manual iter with defer

--- golang/iter/simple1_defer.go
+++ golang/iter/simple1_defer.go
@@ -15,9 +15,43 @@ func SimpleIter() iter.Seq[int] {
 	}
 }
 
-func main() {
+type Iter[E any] func(body func(index int, value E))
+
+func Backward[S ~[]E, E any](s S) Iter[E] {
+	return func(body func(int, E)) {
+		for i := len(s) - 1; i >= 0; i-- {
+			body(i, s[i])
+		}
+	}
+}
+
+func iterRange() {
 	for i := range SimpleIter() {
 		defer func() {log.Printf("defer: %v", i)}()
+		log.Println("Before print i")
 		log.Println(i)
+		log.Println("After print i")
 	}
+	log.Println("Done in iterRange func")
+}
+
+func iterManual() {
+	// Использование
+	backwardIter := Backward([]int{1,2,3})
+
+	backwardIter(func(index int, value int) {
+		defer func(){log.Printf("defer: %v", value)}()
+
+		log.Println("Before print i")
+		log.Println(value)
+		log.Println("After print i")
+	})
+
+	log.Println("Done in iterManual func")
+}
+
+func main() {
+	iterRange()
+	log.Println("------------------")
+	iterManual()
 }
