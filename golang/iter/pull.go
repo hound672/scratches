5f7f39ad3d85ec083d62bb101c61ca96385e556c
hound672 <hound672@gmail.com>
2024-08-19 17:56:53 +0300

    pull iter + simple

--- golang/iter/pull.go
+++ golang/iter/pull.go
@@ -3,8 +3,18 @@ package main
 import (
 	"fmt"
 	"iter"
+	"log"
 	"math/rand"
 )
+
+func SimpleIter() iter.Seq[int] {
+	log.Println("Before")
+	return func(yield func(int) bool) {
+		yield(1)
+		yield(2)
+		yield(3)
+	}
+}
 
 func main() {
 	randIntGen := iter.Seq[int](func(yield func(int) bool) {
@@ -25,5 +35,30 @@ func main() {
 			break
 		}
 		fmt.Printf("[%d] %d\n", i, v)
+	}
+
+	fmt.Println("========================")
+
+	// push
+	fmt.Printf("Push")
+	for i := range SimpleIter() {
+		fmt.Printf("i: %d\n", i)
+	}
+
+	// pull
+	fmt.Println("Pull")
+	simpleIterPull, stop := iter.Pull(SimpleIter())
+
+	for i := range 5 {
+		v, ok := simpleIterPull()
+		if !ok {
+			fmt.Println("not ok")
+			return
+		}
+		fmt.Printf("i: %d; v: %d\n", i, v)
+
+		if i == 1 {
+			stop()
+		}
 	}
 }
