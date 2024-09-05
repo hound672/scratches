84a507e6f00ab2b4b25bc3d68b353af1ba72a806
hound672 <hound672@gmail.com>
2024-09-05 12:16:13 +0300

    iter

--- golang/iter/simple1.go
+++ golang/iter/simple1.go
@@ -8,14 +8,25 @@ import (
 func SimpleIter() iter.Seq[int] {
 	log.Println("Before")
 	return func(yield func(int) bool) {
-		yield(1)
-		yield(2)
-		yield(3)
+		for cnt := range 10 {
+			if !yield(cnt) {
+				log.Println("Cancled?")
+				return
+			}
+			log.Println("After")
+		}
 	}
 }
 
 func main() {
+	cnt := 0
 	for i := range SimpleIter() {
+		continue
 		log.Println(i)
+		cnt++
+		if cnt >= 2 {
+			break
+		}
+		continue
 	}
 }
