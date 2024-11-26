f2cdf08bcb712ecd2de86f69385e0d637fa97744
Vasilii Bliznetsov <hound672@gmail.com>
2024-11-26 09:26:03 +0300

    errors

--- golang/errors/wrap.go
+++ golang/errors/wrap.go
@@ -11,12 +11,16 @@ type SomeError struct {
 	Val string
 }
 
+
 func (e *SomeError) Error() string {
 	return fmt.Sprintf("some error: %v", e.Val)
 }
 
+var ErrSmth = errors.New("something went wrong")
+
 func rErr() error {
 	return &SomeError{Val: "rErr"}
+	//return ErrSmth
 }
 
 func rErr2() error {
@@ -28,15 +32,28 @@ func rErr2() error {
 	return nil
 }
 
-func main() {
+func rErr3() error {
 	err := rErr2()
+	if err != nil {
+		return fmt.Errorf("[WRAPPED] rErr3: %w", err)
+	}
+
+	return nil
+}
+
+func main() {
+	err := rErr3()
 
 	if err != nil {
 		log.Println(err)
 
 		var e *SomeError
-		if errors.As(err, &e) {
-			log.Println(e.Val)
+
+		switch {
+		case errors.Is(err, ErrSmth):
+			log.Printf("ErrSmth: %v", err)
+		case errors.As(err, &e):
+			log.Printf("ErrAs: %v; val: %v", e, e.Val)
 		}
 	}
 }
